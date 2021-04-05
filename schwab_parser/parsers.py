d10fae6350e946c5cd5fc783a2d39d500e08221c
Marek Wawrzos <marek.28.93@gmail.com>
2021-04-06 01:03:56 +0200

    flake8 suggestions

--- schwab_parser/parsers.py
+++ schwab_parser/parsers.py
@@ -12,18 +12,25 @@ supported_headers = [
     ]
 ]
 
+
 class Parser:
     def __init__(self, header):
         if header not in supported_headers:
-            raise Exception(f'Header not supported. Expected {supported_headers}, found {header}.')
+            raise Exception(
+                f'Header not supported. Expected {supported_headers},'
+                f' found {header}.'
+            )
         self.header = header
 
     def parse(self, row):
         if len(row) < 2:
-            raise Exception(f'Expected more than two columns. Found: {len(row)}')
+            raise Exception(
+                f'Expected more than two columns. Found: {len(row)}'
+            )
         if row[1] == '"Deposit"':
             return DepositParser(self.header, row), None
         raise Exception(f'Not supported row: {row}')
+
 
 class DepositParser(Parser):
     def __init__(self, header, *args):
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,8 +1,6 @@
-import datetime as d
 import pytest
 
 import schwab_parser.parsers as p
-import schwab_parser.entries as e
 
 
 def split_csv(txt):
@@ -12,28 +10,34 @@ def split_csv(txt):
 @pytest.fixture
 def espp_deposit_lines():
     deposit_lines = '''
-"Date","Action","Symbol","Description","Quantity","Fees & Commissions","Disbursement Election","Amount"
+"Date","Action","Symbol","Description","Quantity"''' \
+''',"Fees & Commissions","Disbursement Election","Amount"
 "2001/04/05","Deposit","AAPL","ESPP",42,"","",""
 '''
     return split_csv(deposit_lines)
+
 
 @pytest.fixture
 def parser(espp_deposit_lines):
     header, *_ = espp_deposit_lines
     return p.Parser(header)
 
+
 @pytest.fixture
 def rows(espp_deposit_lines):
     return espp_deposit_lines[1:]
+
 
 def test_wrong_header(espp_deposit_lines):
     header, *rows = espp_deposit_lines
     with pytest.raises(Exception, match='Header not supported'):
         p.Parser([])
 
+
 def test_short_row(parser):
     with pytest.raises(Exception, match='Expected more than two columns'):
         parser.parse([])
+
 
 def test_not_supported(parser, rows):
     row = rows[0]
@@ -41,8 +45,8 @@ def test_not_supported(parser, rows):
     with pytest.raises(Exception, match='Not supported row'):
         parser.parse(row)
 
+
 def test_espp_deposit(parser, rows):
     parser, _ = parser.parse(rows[0])
 
     assert isinstance(parser, p.DepositParser)
-
